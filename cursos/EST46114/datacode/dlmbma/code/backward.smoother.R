7fe068a0805619ba2e9ca16717905f6369371ce6
Juan Carlos Martínez-Ovando <jc.martinez.ovando@gmail.com>
2016-05-19 21:33:50 -0500

    new dlmbma

---
+++ cursos/EST46114/datacode/dlmbma/code/backward.smoother.R
@@ -0,0 +1,43 @@
+backward.smoother <- function(m0,C0,n0,s0,Ft,Gt,yt){
+#	
+#	This function implements the backward-smoother procedure for
+#	individual dinamic linear models
+#
+#	INPUTS:
+#
+#
+#	OUTPUTS:
+#
+#
+#	Author: 	Juan~Carlos Martínez-Ovando
+#	Email:		juan.martinez.ovando@itam.mx
+#				JC.Martinez.Ovando@gmail.com
+#
+
+#	One-step backward
+at <- Gt %*% m0
+Rt <- t(Gt) %*% C0 %*% Gt #+ Wt
+ft <- Ft %*% at
+Qt <- drop(Ft %*% Rt %*% Ft + s0)
+#	Updating...
+et <- drop(yt-ft)
+At <- (Rt %*% Ft) / Qt
+nt <- n0 + 1
+st <- drop(s0 + (s0/nt)*((et^2)/Qt - 1))
+mt <- at + At * et
+colnames(mt) <- colnames(m0)
+rownames(mt) <- rownames(m0)
+Ct <- (st/drop(s0)) * (Rt - (At%*%t(At)) * Qt)
+colnames(Ct) <- rownames(mt)
+rownames(Ct) <- rownames(mt)
+
+# Output
+list(ft=ft,
+	 Qt=Qt,
+	 mt=mt, 
+	 Ct=Ct, 
+	 nt=nt, 
+	 st=st)
+}
+#
+#	END of "forward.filter.R"
